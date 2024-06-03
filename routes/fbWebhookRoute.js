2afb6240bb855aa570d92bf0c1f13c6a5f270337
KINGBOTs1 <ds767365@gmail.com>
2024-06-03 12:23:17 +0800

    new

--- routes/fbWebhookRoute.js
+++ routes/fbWebhookRoute.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
   if (mode && token) {
-    if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+    if (mode === 'subscribe' && token === '123dibo') {
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
     } else {
